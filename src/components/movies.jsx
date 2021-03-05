2dfe8fadf990a3678e93cf2f7c4423512b38acd0
Helio Junior <helios.junior@gmail.com>
2021-03-05 14:35:18 -0300

    Added movies state in Movies component getting data from fakeMovieService

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
-  state = {};
+  state = {
+    movies: getMovies(),
+  };
   render() {
     return (
       <React.Fragment>
