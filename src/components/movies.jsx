cc6caae85c3ac7fe269c215a1b8d1e772e722083
Helio Junior <helios.junior@gmail.com>
2021-03-05 14:06:40 -0300

    Movies component created with sample message

---
+++ src/components/movies.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Movies extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Movie Component</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
