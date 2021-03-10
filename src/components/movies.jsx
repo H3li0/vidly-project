35b9b40f1f16c2d3a12c4f31a9206ccb4304c840
Helio Junior <helios.junior@gmail.com>
2021-03-10 17:13:25 -0300

    Sorting - Extracting a Method

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -46,8 +46,7 @@ class Movies extends Component {
     this.setState({ sortColumn });
   };
 
-  render() {
-    const { length: count } = this.state.movies;
+  getPageData = () => {
     const {
       pageSize,
       currentPage,
@@ -56,8 +55,6 @@ class Movies extends Component {
       movies: allMovies,
     } = this.state;
 
-    if (count === 0) return <p>There are no movies in database.</p>;
-
     const filtered =
       selectedGenre && selectedGenre._id
         ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
@@ -65,6 +62,17 @@ class Movies extends Component {
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
     const movies = paginate(sorted, currentPage, pageSize);
+
+    return { totalCount: filtered.length, data: movies };
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    const { pageSize, currentPage, sortColumn } = this.state;
+
+    if (count === 0) return <p>There are no movies in database.</p>;
+
+    const { totalCount, data: movies } = this.getPageData();
 
     return (
       <div className="row">
@@ -76,7 +84,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>There are {filtered.length} movies in database.</p>
+          <p>There are {totalCount} movies in database.</p>
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
@@ -85,7 +93,7 @@ class Movies extends Component {
             onSort={this.handleSort}
           />
           <Pagination
-            itemsCount={filtered.length}
+            itemsCount={totalCount}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
